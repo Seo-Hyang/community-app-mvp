a816b065e269fab1964a405dfd95d4f0bc440633
Seo-Hyang <hyangaroma@gmail.com>
2025-07-07 17:23:57 +0900

    chore: 전체 공통 배경 색상 적용

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
+import { colors } from "@/styles/shared";
 import { Stack } from "expo-router";
+import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
 
-export default function Layout() {
-  return <Stack />;
+export default function RootLayout() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor={colors.background} />
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: colors.background },
+          contentStyle: { backgroundColor: colors.background },
+        }}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+});
