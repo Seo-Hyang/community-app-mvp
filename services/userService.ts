78aaefb27ae727b38b3969c3135eb0536c1bd483
Seo-Hyang <hyangaroma@gmail.com>
2025-07-07 11:26:31 +0900

    feat: 닉네임 입력 추가

--- services/userService.ts
+++ services/userService.ts
@@ -4,14 +4,17 @@ import { db } from "./firebaseConfig";
 export const createUserDocument = async ({
   uid,
   email,
+  displayName,
 }: {
   uid: string;
   email: string | null;
+  displayName: string;
 }) => {
   const userRef = doc(db, "users", uid);
   await setDoc(userRef, {
     uid,
     email: email ?? "",
+    displayName: displayName,
     createdAt: serverTimestamp(),
   });
 };
